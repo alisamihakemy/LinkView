68fbed653a3a257affffe03f4adf2c04b97fdc01
alisamihakemy <121377434+alisamihakemy@users.noreply.github.com>
2024-10-30 22:29:04 -0700

    minSdk

--- linkView/build.gradle.kts
+++ linkView/build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation ("madar.soft.linkViewer:library:1.1.0")
 
 
 }
